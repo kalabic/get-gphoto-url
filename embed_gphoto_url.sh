feda2a04de71fab5f16a486c62bf480709153ea8
kalabic <kalabic@get-gphoto-url>
2023-05-10 19:31:48 +0200

    Redirect error messages to error output. Make 'embed_gphoto_url' accept and 'photos.app.goo.gl' links and automatically invoke 'get_gphoto_url'.

--- embed_gphoto_url.sh
+++ embed_gphoto_url.sh
@@ -11,25 +11,35 @@
 # 4. (Optional) CSS class attribute for 'img' element.
 
 
-if ! [[ "$1" =~ ^"https://lh3.googleusercontent.com/" ]]; then
-    echo "Expecting argument in format of shared Google Photos URL: https://lh3.googleusercontent.com/..."
+gphoto_url=$1
+
+if [[ "$gphoto_url" =~ ^"https://photos.app.goo.gl/" ]]; then
+    gphoto_url=$(./get_gphoto_url.sh $gphoto_url)
+    result=$?
+    if [ $result -ne 0 ]; then
+        exit 1
+    fi
+fi
+
+if ! [[ "$gphoto_url" =~ ^"https://lh3.googleusercontent.com/" ]]; then
+    echo "Error: Expecting argument in format of shared Google Photos URL: https://lh3.googleusercontent.com/..." 1>&2
     exit 1
 fi
 
 re='^[0-9]+$'
 if ! [[ $2 =~ $re ]] ; then
-   echo "error: Second argument not a number" >&2
+   echo "Error: Second argument not a number" 1>&2
    exit 1
 fi
 
 if ! [[ $3 =~ $re ]] ; then
-   echo "error: Third argument not a number" >&2
+   echo "Error: Third argument not a number" 1>&2
    exit 1
 fi
 
-base_url=$(echo -n $1 | sed 's![^=]*$!!')
+base_url=$(echo -n $gphoto_url | sed 's![^=]*$!!')
 if ! [[ "$base_url" =~ ^"https://lh3.googleusercontent.com/" ]]; then
-    echo "Sorry, creating embed code failed."
+    echo "Error: Sorry, creating embed code failed." 1>&2
     exit 1
 fi
 
@@ -43,5 +53,3 @@ else
 fi
 
 echo "$code"
-
-exit 0
--- get_gphoto_url.sh
+++ get_gphoto_url.sh
@@ -7,24 +7,20 @@
 # On rare occasions parsing response can fail. Just try again and good luck!
 
 
-if [[ "$1" =~ ^"https://photos.app.goo.gl/" ]]; then
-    echo -n "Contacting server..."
-else
-    echo "Expecting argument in format of shared Google Photos URL: https://photos.app.goo.gl/..."
+if ! [[ "$1" =~ ^"https://photos.app.goo.gl/" ]]; then
+    echo "Error: Expecting argument in format of shared Google Photos URL: https://photos.app.goo.gl/..." 1>&2
     exit 1
 fi
 
 page=$(wget --no-cookies -qO- $1)
 result=$?
 if [ $result -ne 0 ]; then
-    echo "Error: Download from provided URL failed."
+    echo "Error: Download from provided URL failed." 1>&2
     exit 1
 fi
 
-if [[ "$page" =~ ^"<!doctype html>" ]]; then
-    echo "parsing response..."
-else
-    echo "Error: HTML format not recognized, unsupported response from server."
+if ! [[ "$page" =~ ^"<!doctype html>" ]]; then
+    echo "Error: HTML format not recognized, unsupported response from server." 1>&2
     exit 1
 fi
 
@@ -32,8 +28,6 @@ link=$(echo -n $page | grep -o "content=\"https://lh3.googleusercontent.com/[A-Z
 if [[ "$link" =~ ^"https://lh3.googleusercontent.com/" ]]; then
     echo "$link"
 else
-    echo "Sorry, gphoto URL not found in response. Trying again can fix the issue!"
+    echo "Error: Sorry, gphoto URL not found in response. Trying again can fix the issue." 1>&2
     exit 1
 fi
-
-exit 0
